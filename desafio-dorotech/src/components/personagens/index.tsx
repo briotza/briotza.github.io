c5b5b1b736642dcd7bfa66dd6384417c15214adf
briotza <cianomeliunas@gmail.com>
2024-10-23 20:19:29 -0300

    feat: lógica para armazenar os personagens da API

--- desafio-dorotech/src/components/personagens/index.tsx
+++ desafio-dorotech/src/components/personagens/index.tsx
@@ -3,7 +3,19 @@ import axios from "axios"
 
 
 export default function ListaPersonagens() {
-    return(
+    const [personagens, setPersonagens] = useState([])
+
+    const fetchPersonagens = async () => {
+        try {
+            const resposta = await axios.get('https://rickandmortyapi.com/api/character')
+            setPersonagens(resposta.data.results)
+
+        } catch (error) {
+            console.log('Não foi possível carregar a lista:'), error
+        }
+    }
+
+    return (
         <h1 className="">Lista de Personagens</h1>
     )
 }
