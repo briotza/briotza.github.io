82e648957747047725fcc9f68768ca9bed145339
briotza <cianomeliunas@gmail.com>
2024-10-23 22:33:26 -0300

    feat: filtro de nome

--- desafio-dorotech/src/components/personagens/index.tsx
+++ desafio-dorotech/src/components/personagens/index.tsx
@@ -18,9 +18,9 @@ export default function ListaPersonagens() {
     const [filtroGenero, setFiltroGenero] = useState<string>('')
     const [filtroStatus, setFiltroStatus] = useState<string>('')
     const [filtroEspecie, setFiltroEspecie] = useState<string>('')
+    const [filtroNome, setFiltroNome] = useState<string>('')
 
 
-    
     const fetchPersonagens = async () => {
         try {
             const todos = []
@@ -59,10 +59,11 @@ export default function ListaPersonagens() {
 
     const filtrarPersonagens = () => {
         return personagens.filter(personagem => {
-            const generoMatch = filtroGenero ? personagem.gender === filtroGenero : true;
-            const statusMatch = filtroStatus ? personagem.status === filtroStatus : true;
-            const especieMatch = filtroEspecie ? personagem.species === filtroEspecie : true;
-            return generoMatch && statusMatch && especieMatch;
+            const generoMatch = filtroGenero ? personagem.gender === filtroGenero : true
+            const statusMatch = filtroStatus ? personagem.status === filtroStatus : true
+            const especieMatch = filtroEspecie ? personagem.species === filtroEspecie : true
+            const nomeMatch = filtroNome ? personagem.name.toLowerCase().includes(filtroNome.toLowerCase()) : true
+            return nomeMatch && generoMatch && statusMatch && especieMatch
         });
     }
 
@@ -70,50 +71,54 @@ export default function ListaPersonagens() {
 
 
     //Lógica para exibição de personagens
-    const inicio = (pagina-1) * itens
+    const inicio = (pagina - 1) * itens
     const fim = inicio + itens
-    const exibidos = personagensFiltrados.slice(inicio,fim)
+    const exibidos = personagensFiltrados.slice(inicio, fim)
 
     //Botões de navegação de página
     const proximo = () => {
-        if(pagina < Math.ceil(personagensFiltrados.length/itens)){
-            setPagina(pagina+1)
+        if (pagina < Math.ceil(personagensFiltrados.length / itens)) {
+            setPagina(pagina + 1)
         }
     }
     const anterior = () => {
-        if(pagina > 1){
-            setPagina(pagina-1)
+        if (pagina > 1) {
+            setPagina(pagina - 1)
         }
     }
 
-    
+
 
     return (
         <div>
             <h1 className="">Lista de Personagens</h1>
 
-            <label>Filtro de Gênero:</label>
-                <select onChange={(e) => setFiltroGenero(e.target.value)} value={filtroGenero}>
-                    <option value="">Todos</option>
-                    {opcoesGenero.map(genero => (
-                        <option key={genero} value={genero}>{genero}</option>
-                    ))}
-                </select>
-                <label>Filtro de Status:</label>
-                <select onChange={(e) => setFiltroStatus(e.target.value)} value={filtroStatus}>
-                    <option value="">Todos</option>
-                    {opcoesStatus.map(status => (
-                        <option key={status} value={status}>{status}</option>
-                    ))}
-                </select>
+            <input type="text" value={filtroNome} onChange={(e) => setFiltroNome(e.target.value)} className="w-[300px]" placeholder="Digite o nome do personagem" />
 
-                <label>Filtro de Espécie:</label>
-                <select onChange={(e) => setFiltroEspecie(e.target.value)} value={filtroEspecie}>
-                    <option value="">Todos</option>
-                    {opcoesEspecie.map(especie => (
-                        <option key={especie} value={especie}>{especie}</option>
-                    ))}
-                </select>
+            <label>Filtro de Gênero:</label>
+            <select onChange={(e) => setFiltroGenero(e.target.value)} value={filtroGenero}>
+                <option value="">Todos</option>
+                {opcoesGenero.map(genero => (
+                    <option key={genero} value={genero}>{genero}</option>
+                ))}
+            </select>
+
+            <label>Filtro de Status:</label>
+            <select onChange={(e) => setFiltroStatus(e.target.value)} value={filtroStatus}>
+                <option value="">Todos</option>
+                {opcoesStatus.map(status => (
+                    <option key={status} value={status}>{status}</option>
+                ))}
+            </select>
+
+            <label>Filtro de Espécie:</label>
+            <select onChange={(e) => setFiltroEspecie(e.target.value)} value={filtroEspecie}>
+                <option value="">Todos</option>
+                {opcoesEspecie.map(especie => (
+                    <option key={especie} value={especie}>{especie}</option>
+                ))}
+            </select>
+
             {/* Lista de quantidade de itens */}
             <select onChange={handleItensPagina} value={itens}>
                 <option value={5}>5</option>
@@ -121,18 +126,20 @@ export default function ListaPersonagens() {
                 <option value={20}>20</option>
             </select>
             <ul>
+
                 {/* Listagem de personagens */}
                 {exibidos.map(personagem => (
                     <li key={personagem.id}>
                         <Link to={`/personagem/${personagem.id}`}>
-                            {personagem.id} - 
-                            {personagem.name} - 
+                            {personagem.id} -
+                            {personagem.name} -
                             {personagem.status}
                             <img src={personagem.image} className="w-20" />
                         </Link>
                     </li>
                 ))}
             </ul>
+
             {/* Navegação entre páginas */}
             <button onClick={anterior}>
                 Anterior
