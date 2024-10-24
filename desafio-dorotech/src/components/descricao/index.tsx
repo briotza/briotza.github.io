4b62d708ac1c1f0184bf858f062eef50622849ee
briotza <cianomeliunas@gmail.com>
2024-10-23 21:46:49 -0300

    feat: definindo types para descrição

--- desafio-dorotech/src/components/descricao/index.tsx
+++ desafio-dorotech/src/components/descricao/index.tsx
@@ -2,23 +2,28 @@ import { useEffect, useState } from "react"
 import { Personagem } from "../../types/personagens.types"
 import { useParams } from "react-router-dom"
 import axios from "axios"
+import { DescricaoPersonagem } from "../../types/descricao.types"
 
 export default function Descricao(){
-    const [personagem, setPersonagem] = useState<Personagem[]>([])
+    const [personagem, setPersonagem] = useState<DescricaoPersonagem | null>(null)
     const id = useParams<{id:string}>()
 
     useEffect(() => {
         const fetchPersonagem = async() => {
             const resposta = await axios.get(`ttps://rickandmortyapi.com/api/character/${id}`)
-            setPersonagem(resposta.data.results)
+            setPersonagem(resposta.data)
         }
 
         fetchPersonagem()
     },[id])
 
     return(
+        <div>
         <p>Descrição do personagem</p>
+        <img src={personagem.image} />
+        {personagem.name}
 
+        </div>
 
     )
 }
