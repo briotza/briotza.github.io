56c854132a3e5a28a1f62bdc53344a50cfc6a41c
briotza <cianomeliunas@gmail.com>
2024-10-25 11:30:24 -0300

    style: ajuste nos estilos da home

--- desafio-dorotech/src/pages/home.tsx
+++ desafio-dorotech/src/pages/home.tsx
@@ -5,10 +5,10 @@ import portal from '../assets/green-portal.png'
 export default function Home() {
     return (
         <div className="h-full flex flex-col bg-custom-image bg-cover bg-center items-center">
-            <div className="flex flex-row w-screen items-center p-3">
-                <img src={logo} className="max-w-[300px]" />
+            <div className="flex flex-row w-screen items-center justify-center gap-4 h-[140px]">
+                <h1 className=" font-creepster text-[#12ABC9] text-4xl text-outline mb-5">Lista de Personagens</h1>
             </div>
-            <h1 className=" font-creepster text-[#12ABC9] text-3xl text-outline mb-5">Lista de Personagens</h1>
+            <img src={logo} className="max-w-[300px] absolute right-0 mr-4 mt-4" />
             <ListaPersonagens />
             <img src={portal} className="w-[800px]" />
         </div>
