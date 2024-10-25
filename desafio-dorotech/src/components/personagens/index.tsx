7ce58a2b0d2f1cb79348db625241984c6ec94ac2
briotza <cianomeliunas@gmail.com>
2024-10-25 12:47:49 -0300

    style: alterei altura da lista de personagens

--- desafio-dorotech/src/components/personagens/index.tsx
+++ desafio-dorotech/src/components/personagens/index.tsx
@@ -92,7 +92,7 @@ export default function ListaPersonagens() {
 
 
     return (
-        <div className="h-[590px]">
+        <div className="h-[1100px]">
             {/* Filtro de nome */}
             <input type="text" value={filtroNome} onChange={(e) => setFiltroNome(e.target.value)} className="w-[300px]" placeholder="Digite o nome do personagem" />
 
@@ -131,7 +131,7 @@ export default function ListaPersonagens() {
             </select>
 
             {/* Listagem de personagens */}
-            <ul className="grid grid-cols-4 gap-3 mt-3">
+            <ul className="grid grid-cols-2 gap-3 mt-3">
                 {exibidos.map(personagem => (
                     <li key={personagem.id} className="p-2 bg-white rounded-xl">
                         <Link to={`/personagem/${personagem.id}`} className="flex flex-row gap-2">
