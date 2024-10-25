aab8a34465b874fe2f57721f47c3537ee0417c6a
briotza <cianomeliunas@gmail.com>
2024-10-25 11:44:32 -0300

    feat: inseri imagens do título

--- desafio-dorotech/src/pages/home.tsx
+++ desafio-dorotech/src/pages/home.tsx
@@ -1,14 +1,18 @@
 import ListaPersonagens from "../components/personagens";
 import logo from '../assets/logo.png'
 import portal from '../assets/green-portal.png'
+import morty from '../assets/morty.png'
+import rick from '../assets/rick.png'
 
 export default function Home() {
     return (
         <div className="h-full flex flex-col bg-custom-image bg-cover bg-center items-center">
-            <div className="flex flex-row w-screen items-center justify-center gap-4 h-[140px]">
+            <div className="flex flex-row w-screen items-center justify-center gap-4 h-[160px]">
+                <img src={rick} className="max-h-[130px] left-0 ml-4 mt-4" />
                 <h1 className=" font-creepster text-[#12ABC9] text-4xl text-outline mb-5">Lista de Personagens</h1>
+                <img src={morty} className="max-h-[130px] right-0 mr-4 mt-4" />
             </div>
-            <img src={logo} className="max-w-[300px] absolute right-0 mr-4 mt-4" />
+
             <ListaPersonagens />
             <img src={portal} className="w-[800px]" />
         </div>
