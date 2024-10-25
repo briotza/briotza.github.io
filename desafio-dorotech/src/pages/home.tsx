d6a9d7f711c18f203aa0e148c3ecc16bdf411e40
briotza <cianomeliunas@gmail.com>
2024-10-25 12:48:06 -0300

    style: alterei topo da home

--- desafio-dorotech/src/pages/home.tsx
+++ desafio-dorotech/src/pages/home.tsx
@@ -7,7 +7,7 @@ import rick from '../assets/rick.png'
 export default function Home() {
     return (
         <div className="h-full flex flex-col bg-custom-image bg-cover bg-center items-center">
-            <div className="flex flex-row w-screen items-center justify-center gap-4 h-[160px]">
+            <div className="flex flex-row w-screen items-center justify-center gap-4 h-[160px] border-b-2 bg-black">
                 <img src={rick} className="max-h-[125px] left-0 ml-4 mt-4" />
                 <h1 className=" font-creepster text-[#A8D81B] text-4xl text-outline mb-5">Lista de Personagens</h1>
                 <img src={morty} className="max-h-[125px] right-0 mr-4 mt-4" />
