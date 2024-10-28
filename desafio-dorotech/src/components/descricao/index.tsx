bcae9b0447599bf3a536586cd02938525b94d7ed
briotza <cianomeliunas@gmail.com>
2024-10-28 20:51:19 -0300

    feat: preenchi modal com informações do personagem

--- desafio-dorotech/src/components/descricao/index.tsx
+++ desafio-dorotech/src/components/descricao/index.tsx
@@ -26,11 +26,22 @@ export default function Descricao({ id, onClose }: DetalhesModal) {
     return (
         <Dialog.Root open={!!id} onOpenChange={() => onClose()}>
             <Dialog.Overlay className="fixed inset-0 bg-black opacity-30" />
-            <Dialog.Content className="fixed bg-white p-4 rounded-lg shadow-lg max-w-md mx-auto inset-0 m-auto">
-                <Dialog.Title>Teste</Dialog.Title>
-                <Dialog.Description />
+            <Dialog.Content className="fixed bg-black p-5 rounded-lg shadow-lg max-w-2xl inset-0 m-auto text-xl text-white h-[400px] flex flex-col">
+                <Dialog.Title className="mb-2 text-2xl">{personagem?.name}</Dialog.Title>
+                <div className="flex-grow">
+                    <Dialog.Description className="flex flex-row items-center">
+                        <img src={personagem?.image} className="h-[220px]" />
+                        <div className="ml-4">
+                            <span>Espécie</span><p className="text-sm">{personagem?.species}</p>
+                            <span>Status</span><p className="text-sm">{personagem?.status}</p>
+                            <span>Gênero</span><p className="text-sm">{personagem?.gender}</p>
+                            <span>Origem</span><p className="text-sm">{personagem?.origin.name}</p>
+                            <span>Localização</span><p className="text-sm">{personagem?.location.name}</p>
+                        </div>
+                    </Dialog.Description>
+                </div>
                 <Dialog.Close asChild>
-                    <button onClick={onClose} className="mt-4 px-4 py-2 bg-blue-500 text-white rounded">Fechar</button>
+                    <button onClick={onClose} className="text-black mt-4 self-end px-4 py-2 bg-[#8EDC23] rounded">Fechar</button>
                 </Dialog.Close>
             </Dialog.Content>
         </Dialog.Root>
