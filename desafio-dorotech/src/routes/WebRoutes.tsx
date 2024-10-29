a27c5273495b2cdd18930a6a40decd29733ed770
briotza <cianomeliunas@gmail.com>
2024-10-28 23:43:19 -0300

    chore: apaguei rota não utilizada

--- desafio-dorotech/src/routes/WebRoutes.tsx
+++ desafio-dorotech/src/routes/WebRoutes.tsx
@@ -1,13 +1,11 @@
 import { Route, BrowserRouter as Router, Routes } from 'react-router-dom'
 import Home from '../pages/home'
-import PaginaDescricao from '../pages/descricao'
 
 export default function WebRoutes(){
     return(
         <Router>
             <Routes>
                 <Route path='/' element={<Home />} />
-                <Route path='/personagem/:id' element={<PaginaDescricao />} />
             </Routes>
         </Router>
     )
